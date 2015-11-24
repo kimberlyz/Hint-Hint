ffb2f8d3baa4a8a5dd4cb3b4f6db914bad77573b
Kimberly Zai <kimberlyz621@gmail.com>
2015-11-24 10:48:36 -0800

    func didSelectDayView is now called in MainViewController.

--- MainViewController.swift
+++ MainViewController.swift
@@ -8,15 +8,8 @@
 
 import UIKit
 import CVCalendar
-import CoreBluetooth
-import Foundation
 
-protocol MainViewControllerDelegate : Any {
-    func onDeviceConnectionChange(peripheral:CBPeripheral)
-}
-
-
-class MainViewController: UIViewController, MainViewControllerDelegate {
+class MainViewController: UIViewController {
 
     @IBOutlet weak var menuView: CVCalendarMenuView!
     @IBOutlet weak var calendarView: CVCalendarView!
@@ -27,70 +20,11 @@ class MainViewController: UIViewController, MainViewControllerDelegate {
     
     var selectedDay:DayView!
     
-    
-    enum ConnectionStatus:Int {
-        case Idle = 0
-        case Scanning
-        case Connected
-        case Connecting
-    }
-    
-    var connectionMode:ConnectionMode = ConnectionMode.None
-    var connectionStatus:ConnectionStatus = ConnectionStatus.Idle
-    var deviceListViewController:DeviceListViewController!
-    var controllerViewController:ControllerViewController!
-    var delegate:MainViewControllerDelegate?
-    
-    
-    
-    private var connectionTimer:NSTimer?
-    private let cbcmQueue = dispatch_queue_create("com.adafruit.bluefruitconnect.cbcmqueue", DISPATCH_QUEUE_CONCURRENT)
-    private var currentPeripheral:BLEPeripheral?
-    private var currentAlertView:UIAlertController?
-    private var cm:CBCentralManager?
-    private var firmwareUpdater : FirmwareUpdater?
-    
-    func centralManager()->CBCentralManager{
-        
-        return cm!;
-        
-    }
-    
-    //for Objective-C delegate compatibility
-    func setDelegate(newDelegate:AnyObject){
-        
-        if newDelegate.respondsToSelector(Selector("onDeviceConnectionChange:")){
-            delegate = newDelegate as? MainViewControllerDelegate
-        }
-        else {
-            printLog(self, funcName: "setDelegate", logString: "failed to set delegate")
-        }
-        
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         monthLabel.text = CVDate(date: NSDate()).globalDescription
         // Do any additional setup after loading the view.
-        
-        self.delegate = self
-        
-        // Create core bluetooth manager on launch
-        if (cm == nil) {
-            cm = CBCentralManager(delegate: self, queue: cbcmQueue)
-            
-            connectionMode = ConnectionMode.None
-            connectionStatus = ConnectionStatus.Idle
-            currentAlertView = nil
-        }
-        
-        //refresh updates for DFU
-        FirmwareUpdater.refreshSoftwareUpdatesDatabase()
-        let areAutomaticFirmwareUpdatesEnabled = NSUserDefaults.standardUserDefaults().boolForKey("updatescheck_preference");
-        if (areAutomaticFirmwareUpdatesEnabled) {
-            firmwareUpdater = FirmwareUpdater()
-        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -104,6 +38,20 @@ class MainViewController: UIViewController, MainViewControllerDelegate {
         self.calendarView.commitCalendarViewUpdate()
         self.menuView.commitMenuViewUpdate()
     }
+   }
+
+extension MainViewController: CVCalendarViewDelegate, CVCalendarMenuViewDelegate {
+    
+    /// Required method to implement!
+    func presentationMode() -> CalendarMode {
+        return .MonthView
+    }
+    
+    /// Required method to implement!
+    func firstWeekday() -> Weekday {
+        return .Sunday
+    }
+    
     
     // MARK: Optional methods
     
@@ -115,7 +63,7 @@ class MainViewController: UIViewController, MainViewControllerDelegate {
         return true // Default value is true
     }
     
-    func didSelectDayView(dayView: CVCalendarDayView, animationDidFinish: Bool) {
+    func didSelectDayView(dayView: CVCalendarDayView) {
         print("\(dayView.date.commonDescription) is selected!")
         selectedDay = dayView
     }
@@ -157,39 +105,6 @@ class MainViewController: UIViewController, MainViewControllerDelegate {
             self.view.insertSubview(updatedMonthLabel, aboveSubview: self.monthLabel)
         }
     }
-    
-    // MARK : Bluetooth Connection
-    
-    func abortConnection() {
-        
-        connectionTimer?.invalidate()
-        
-        if (cm != nil) && (currentPeripheral != nil) {
-            cm!.cancelPeripheralConnection(currentPeripheral!.currentPeripheral)
-        }
-        
-        currentPeripheral = nil
-        
-        connectionMode = ConnectionMode.None
-        connectionStatus = ConnectionStatus.Idle
-    }
-    
-    
-
-
-}
-
-extension MainViewController: CVCalendarViewDelegate, CVCalendarMenuViewDelegate {
-    
-    /// Required method to implement!
-    func presentationMode() -> CalendarMode {
-        return .MonthView
-    }
-    
-    /// Required method to implement!
-    func firstWeekday() -> Weekday {
-        return .Sunday
-    }
 }
 
 // MARK: - CVCalendarViewAppearanceDelegate
@@ -202,175 +117,4 @@ extension MainViewController: CVCalendarViewAppearanceDelegate {
     func spaceBetweenDayViews() -> CGFloat {
         return 2
     }
-}
-
-//MARK: CBCentralManagerDelegate methods
-
-extension MainViewController: CBCentralManagerDelegate {
-    
-    func centralManagerDidUpdateState(central: CBCentralManager) {
-        
-        if (central.state == CBCentralManagerState.PoweredOn){
-            
-            //respond to powered on
-        }
-            
-        else if (central.state == CBCentralManagerState.PoweredOff){
-            
-            //respond to powered off
-        }
-        
-    }
-    
-    
-    func centralManager(central: CBCentralManager, didDiscoverPeripheral peripheral: CBPeripheral, advertisementData: [String : AnyObject], RSSI: NSNumber) {
-        
-        if connectionMode == ConnectionMode.None {
-            dispatch_sync(dispatch_get_main_queue(), { () -> Void in
-                if self.deviceListViewController == nil {
-                    self.createDeviceListViewController()
-                }
-                self.deviceListViewController.didFindPeripheral(peripheral, advertisementData: advertisementData, RSSI:RSSI)
-            })
-            
-
-            
-        }
-    }
-    
-    
-    func centralManager(central: CBCentralManager, didConnectPeripheral peripheral: CBPeripheral) {
-        
-        if (delegate != nil) {
-            delegate!.onDeviceConnectionChange(peripheral)
-        }
-        
-        //Connecting in DFU mode, discover specific services
-        if connectionMode == ConnectionMode.DFU {
-            peripheral.discoverServices([dfuServiceUUID(), deviceInformationServiceUUID()])
-        }
-        
-        if currentPeripheral == nil {
-            printLog(self, funcName: "didConnectPeripheral", logString: "No current peripheral found, unable to connect")
-            return
-        }
-        
-        
-        if currentPeripheral!.currentPeripheral == peripheral {
-            
-            printLog(self, funcName: "didConnectPeripheral", logString: "\(peripheral.name)")
-            
-            //Discover Services for device
-            if((peripheral.services) != nil){
-                printLog(self, funcName: "didConnectPeripheral", logString: "Did connect to existing peripheral \(peripheral.name)")
-                currentPeripheral!.peripheral(peripheral, didDiscoverServices: nil)  //already discovered services, DO NOT re-discover. Just pass along the peripheral.
-            }
-            else {
-                currentPeripheral!.didConnect(connectionMode)
-            }
-            
-        }
-    }
-    
-    
-    func centralManager(central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: NSError?) {
-        
-        //respond to disconnection
-        
-        if (delegate != nil) {
-            delegate!.onDeviceConnectionChange(peripheral)
-        }
-        
-        if connectionMode == ConnectionMode.DFU {
-            connectionStatus = ConnectionStatus.Idle
-            return
-        }
-        else if connectionMode == ConnectionMode.Controller {
-            controllerViewController.showNavbar()
-        }
-        
-        printLog(self, funcName: "didDisconnectPeripheral", logString: "")
-        
-        if currentPeripheral == nil {
-            printLog(self, funcName: "didDisconnectPeripheral", logString: "No current peripheral found, unable to disconnect")
-            return
-        }
-        
-        //if we were in the process of scanning/connecting, dismiss alert
-        if (currentAlertView != nil) {
-            uartDidEncounterError("Peripheral disconnected")
-        }
-        
-        //if status was connected, then disconnect was unexpected by the user, show alert
-        if  connectionStatus == ConnectionStatus.Connected {
-            
-            printLog(self, funcName: "centralManager:didDisconnectPeripheral", logString: "unexpected disconnect while connected")
-            
-            //return to main view
-            dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                self.respondToUnexpectedDisconnect()
-            })
-        }
-            
-            // Disconnected while connecting
-        else if connectionStatus == ConnectionStatus.Connecting {
-            
-            abortConnection()
-            
-            printLog(self, funcName: "centralManager:didDisconnectPeripheral", logString: "unexpected disconnect while connecting")
-            
-            //return to main view
-            dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                self.respondToUnexpectedDisconnect()
-            })
-            
-        }
-        
-        connectionStatus = ConnectionStatus.Idle
-        connectionMode = ConnectionMode.None
-        currentPeripheral = nil
-        
-        // Dereference mode controllers
-        dereferenceModeController()
-        
-    }
-    
-    
-    func centralManager(central: CBCentralManager, didFailToConnectPeripheral peripheral: CBPeripheral, error: NSError?) {
-        
-        if (delegate != nil) {
-            delegate!.onDeviceConnectionChange(peripheral)
-        }
-        
-    }
-    
-    
-    func respondToUnexpectedDisconnect() {
-        
-        //display disconnect alert
-        let alert = UIAlertView(title:"Disconnected",
-            message:"BlE device disconnected",
-            delegate:self,
-            cancelButtonTitle:"OK")
-        
-        let note = UILocalNotification()
-        note.fireDate = NSDate().dateByAddingTimeInterval(0.0)
-        note.alertBody = "BLE device disconnected"
-        note.soundName =  UILocalNotificationDefaultSoundName
-        UIApplication.sharedApplication().scheduleLocalNotification(note)
-        
-        alert.show()
-        
-        
-    }
-    
-    
-    func dereferenceModeController() {
-        
-        uartViewController = nil
-        deviceInfoViewController = nil
-        controllerViewController = nil
-    }
-    
-
 }
